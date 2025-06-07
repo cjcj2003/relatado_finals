b66a58cd8fe01fe5bb6de8d62f68356fa5bb7227
cjcj2003 <159215596+cjcj2003@users.noreply.github.com>
2025-06-07 18:47:35 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABNmZqxXgxRQzZh0rij2ZjNwRiqt8HUIY",
+  authDomain: "crud-f3367.firebaseapp.com",
+  databaseURL: "https://crud-f3367-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-f3367",
+  storageBucket: "crud-f3367.firebasestorage.app",
+  messagingSenderId: "744805027199",
+  appId: "1:744805027199:web:7b462f03185bc4ff7cde4a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
